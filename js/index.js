e4b4fd9a197508f1ebf647623073b198b5e37929
tauan-longaretti <tauanlongaretti@gmail.com>
2019-11-05 21:05:07 -0500

    added missing event listeners

--- js/index.js
+++ js/index.js
@@ -27,15 +27,59 @@ el.addEventListener('wheel', zoom);
 
 // Load
 
-// window.addEventListener('load', (event) => {
-//     alert('Welcome to Fun Bus Travel Agency!')
-// });
-
-// Focus
-
-const focus = document.querySelector(".main-navigation");
-focus.addEventListener('focus', (event) => {
-    event.target.style.background = '#d1d1d1';
+window.addEventListener('load', (event) => {
+    alert('Welcome to Fun Bus Travel Agency!')
 });
 
+// Mouse Enter
+const italicize = document.querySelector(".footer");
+italicize.addEventListener('mouseenter', () => {
+  italicize.style.fontStyle = "italic";  
+})
+
+italicize.addEventListener('mouseleave', () => {
+  italicize.style.fontStyle = "normal";
+})
+
+// Click
+const border = document.querySelector('img');
+border.addEventListener('click', () => {
+    border.style.border = '2px solid #17A2B8';
+});
+
+// Mouse Out
+
+let noBorder = document.querySelector('img');
+noBorder.addEventListener('mouseout', function(event) {
+  event.target.style.border = 'none';
+});
+
+// Focus
+const focused = document.querySelector('a');
+
+focused.addEventListener('focus', (event) => {
+  event.target.style.color = 'red';    
+});
+
+// Blur
+focused.addEventListener('blur', (event) => {
+  event.target.style.opacity = '0.5';    
+});
+
+// Double Click
+const bold = document.querySelector('html');
+bold.addEventListener("dblclick", (event) => {
+  event.target.style.transform = "scale(1.2)";
+});
+
+// Resize
+const heightOutput = document.querySelector('h4');
+const widthOutput = document.querySelector('.btn');
+
+function reportWindowSize() {
+  heightOutput.textContent = window.innerHeight;
+  widthOutput.textContent = window.innerWidth;
+}
+
+window.addEventListener('resize', reportWindowSize);
 
